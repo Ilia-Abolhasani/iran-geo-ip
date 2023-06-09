0d95d40c2d13fc08226f103e9301bf01e7072b3c
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-06-10 02:22:59 +0330

    init

---
+++ generate_db.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw( say );
+
+use MaxMind::DB::Writer::Tree;
+
+my $filename = 'users.mmdb';
+
+# Your top level data structure will always be a map (hash).  The MMDB format
+# is strongly typed.  Describe your data types here.
+# See https://metacpan.org/pod/MaxMind::DB::Writer::Tree#DATA-TYPES
+
+my %types = (
+	environments => [ 'array', 'utf8_string' ],
+	expires      => 'uint32',
+	name         => 'utf8_string',
+);
+
+my $tree = MaxMind::DB::Writer::Tree->new(
+
+	# "database_type" is some arbitrary string describing the database.  At
+	# MaxMind we use strings like 'GeoIP2-City', 'GeoIP2-Country', etc.
+	database_type => 'My-IP-Data',
+
+	# "description" is a hashref where the keys are language names and the
+	# values are descriptions of the database in that language.
+	description =>{ en => 'My database of IP data', fr => "Mon Data d'IP", },
+
+	# "ip_version" can be either 4 or 6
+	ip_version => 4,
+
+	# add a callback to validate data going in to the database
+	map_key_type_callback => sub { $types{ $_[0] } },
+
+	# "record_size" is the record size in bits.  Either 24, 28 or 32.
+	record_size => 24,
+);
+
+my %address_for_employee = (
+	'123.125.71.29/32' => {
+		environments => [ 'development', 'staging', 'production' ],
+		expires      => 86400,
+		name         => 'Jane',
+	},
+	'8.8.8.8/28' => {
+		environments => [ 'development', 'staging' ],
+		expires      => 3600,
+		name         => 'Klaus',
+	},
+);
+
+for my $network ( keys %address_for_employee ) {
+	$tree->insert_network( $network, $address_for_employee{$network} );
+}
+
+# Write the database to disk.
+open my $fh, '>:raw', $filename;
+$tree->write_tree( $fh );
+close $fh;
+
+say "$filename has now been created";
